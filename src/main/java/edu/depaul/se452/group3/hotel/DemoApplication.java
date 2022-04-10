e2c08ebed371216c1d2f6e3ddff478317d332c0c
Guy <guylevi@hotmail.com>
2022-04-10 08:29:23 -0500

    Moved .java files to different package
    moved from com.example to edu.depaul

--- src/main/java/edu/depaul/se452/group3/hotel/DemoApplication.java
+++ src/main/java/edu/depaul/se452/group3/hotel/DemoApplication.java
@@ -1,13 +1,19 @@
-package com.example.demo;
+package edu.depaul.se452.group3.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public void hello() {
+		System.out.println("*** Hello Group 3 ***");
 	}
 
 }
--- src/test/java/edu/depaul/se452/group3/hotel/DemoApplicationTests.java
+++ src/test/java/edu/depaul/se452/group3/hotel/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package edu.depaul.se452.group3.hotel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
