7ee6d81f8844df32a359254c4c77a9980a63789a
Guy <guyguy2@gmail.com>
2022-05-02 14:06:00 -0500

    refactor customer and room data

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -33,7 +33,7 @@ DROP TABLE IF EXISTS customers;
 
 CREATE SEQUENCE hibernate_sequence START WITH 100 INCREMENT BY 1;
 
-CREATE TABLE customers (
+CREATE TABLE customer (
   id INT AUTO_INCREMENT,    
     CUST_ID VARCHAR(10),
     name VARCHAR(50),
