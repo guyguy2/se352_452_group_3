87ca15dc54584ed4c3ab1a5e6cd89d4e2dcdd4d4
Jessica Solis <jsolis4@depaul.edu>
2022-05-29 23:06:38 -0500

    removed unused imports

--- src/main/java/edu/depaul/se452/group3/controllers/RoomController.java
+++ src/main/java/edu/depaul/se452/group3/controllers/RoomController.java
@@ -10,8 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
