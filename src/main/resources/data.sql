7a6ac6ab730edbcec7a65baf85baa2ad74641ea8
Guy <guylevi@hotmail.com>
2022-06-04 09:49:04 -0500

    updated one of the prices which didn't round up correctly

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO rooms (number, description, amenities, status, price) VALUES
-    (101, 'Standard Room, 1 Queen Bed, Park View', 'Wheelchair accessible, Bathrobes, Bathtub or Shower, 40-inch Smart TV', 'Available', 335.99),
+    (101, 'Standard Room, 1 Queen Bed, Park View', 'Wheelchair accessible, Bathrobes, Bathtub or Shower, 40-inch Smart TV', 'Available', 335.00),
     (102, 'Standard Room, 1 King Bed, Riverfront View', 'Wheelchair accessible, Bathrobes, Shower, 40-inch Smart TV, Minibar', 'Available', 435.50),
     (103, 'Standard Room, 2 Double Beds, City View', 'Refrigerator, Bathrobes, Shower, 40-inch Smart TV, Minibar', 'Available', 735.00),
     (201, 'Junior Suite, 2 Queen Beds, Park View', 'Sofa, Desk, TV features: remote control/55in/140cm/LCD screen, Non-smoking', 'Available', 643.00),
